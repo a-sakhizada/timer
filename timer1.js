327764f4c649385a137f7f66db04291a72f8060e
a-sakhizada <arezousakhizada@gmail.com>
2022-02-04 20:53:42 -0500

    mini alarm clock that beeps based on the list of seconds sent in the command line

---
+++ timer1.js
@@ -0,0 +1,21 @@
+//Implement an alarm clock / timer which will beep after a specified amount of time has passed. 
+//The user can specify an unlimited number of alarms using command line arguments
+
+
+//console.log('myArgs: ', process.argv.slice(2));
+
+const timer = function(times) {
+
+    for(let second of process.argv.slice(2))
+    {
+
+        if(second >= 0 && !isNaN(second))
+        {
+            second = second * 1000;
+            setTimeout(() => {
+                console.log(" * ");
+            }, second);
+        }
+    }
+}
+timer(process.argv.slice(2));
